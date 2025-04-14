a2411a2ba6372f9fc530be0ca1f960418602cc9c
lucacolo02 <117742765+lucacolo02@users.noreply.github.com>
2025-04-14 12:47:08 +0200

    Ingresso/Uscita funzionante

--- Assignment1/Assignment1.ino
+++ Assignment1/Assignment1.ino
@@ -1,17 +1,18 @@
-#define LED_GREEN D1   // green LED pin
-#define LED_RED D2 //red LED pin
-#define PHOTORESISTOR_IN A0              // photoresistor in
-#define PHOTORESISTOR_OUT D3  //photoresistor out
-#define PHOTORESISTOR_THRESHOLD 450   // turn led on for light values lesser than this
+#define LED_GREEN D1
+#define LED_RED D2
+#define PHOTORESISTOR_IN A0
+#define PHOTORESISTOR_THRESHOLD 450
+#define BUTTON D3
+#define BUTTON_DEBOUNCE_DELAY 20
+
 unsigned int persone = 0;
-unsigned long tempo = 0;
+unsigned long tempoIn = 0;
+unsigned long tempoOut = 0;
+
 void setup() {
-  // set LED pin as outputs
   pinMode(LED_RED, OUTPUT);
   pinMode(LED_GREEN, OUTPUT);
-
-  
-  // turn led off
+  pinMode(BUTTON, INPUT_PULLUP);
   ledOff();
 
   Serial.begin(115200);
@@ -19,40 +20,74 @@ void setup() {
 }
 
 void loop() {
-  static unsigned int lightSensorValueIn;
-  
-  lightSensorValueIn = analogRead(PHOTORESISTOR_IN);   // read analog value (range 0-1023)
-  //lightSensorValueOut = analogRead(PHOTORESISTOR_OUT);   // read analog value (range 0-1023)
-
+  unsigned int lightSensorValueIn = analogRead(PHOTORESISTOR_IN);
   personaIn(lightSensorValueIn);
 
-  
-       
-    
+  if (isButtonPressed() && persone > 0 && (millis() - tempoOut > 1000)) {
+    personaOut();
+  }
 }
-
 
 void ledOff() {
   digitalWrite(LED_RED, LOW);
   digitalWrite(LED_GREEN, LOW);
 }
 
-void personaIn(int lightSensorValueIn ){
-if (lightSensorValueIn <= PHOTORESISTOR_THRESHOLD) {   // high brightness
-    if(millis()-tempo>1000){
-      tempo=millis();
-      Serial.print("Tempo: ");
-      Serial.println(tempo);
-      Serial.print("Persone: ");
-      Serial.println(persone);
-      if (persone<=5){
-        persone=persone+1;
-        digitalWrite(LED_RED, LOW);                               
+void personaIn(int lightSensorValueIn) {
+  if (lightSensorValueIn <= PHOTORESISTOR_THRESHOLD) {
+    if (millis() - tempoIn > 1000) {
+      tempoIn = millis();
+      if (persone < 5) {
+        persone++;
+        Serial.println(">> INGRESSO");
+        Serial.print("Tempo: "); Serial.println(tempoIn);
+        Serial.print("Persone: "); Serial.println(persone);
+        digitalWrite(LED_RED, LOW);
         digitalWrite(LED_GREEN, HIGH);
-      }else{
+        if (persone==5)
+        {
         digitalWrite(LED_RED, HIGH);
-        digitalWrite(LED_GREEN, LOW);  
-      }                          
-  }    
+        digitalWrite(LED_GREEN, LOW);
+        }
+      } else  {
+        digitalWrite(LED_RED, HIGH);
+        digitalWrite(LED_GREEN, LOW);
+      }
     }
+  }
 }
+
+bool isButtonPressed() {
+  static byte lastButtonState = HIGH;
+  static byte buttonState = HIGH;
+  static unsigned long lastDebounceTime = 0;
+
+  byte reading = digitalRead(BUTTON);
+
+  if (reading != lastButtonState) {
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > BUTTON_DEBOUNCE_DELAY) {
+    if (reading != buttonState) {
+      buttonState = reading;
+      if (buttonState == LOW) {
+        return true;
+      }
+    }
+  }
+
+  lastButtonState = reading;
+  return false;
+}
+
+void personaOut() {
+  tempoOut = millis();
+  persone--;
+  Serial.println("<< USCITA");
+  Serial.print("Tempo: "); Serial.println(tempoOut);
+  Serial.print("Persone: "); Serial.println(persone);
+  digitalWrite(LED_RED, LOW);
+  digitalWrite(LED_GREEN, HIGH);
+}
+
