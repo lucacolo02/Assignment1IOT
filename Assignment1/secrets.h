697ff524adc242905a9e0a3452d8c3bb2f9bfea2
pegghe <m.comi15@campus.unimib.it>
2025-04-28 11:49:57 +0200

    fritzing ok

--- Assignment1/secrets.h
+++ Assignment1/secrets.h
@@ -16,3 +16,13 @@
 #define MYSQL_IP {149, 132, 176, 75}            // IP address of the machine running MySQL
 #define MYSQL_USER "MatteoComi"                  // db user
 #define MYSQL_PASS "iot886035"              // db user's password
+
+// InfluxDB cfg
+#define INFLUXDB_URL "http://149.132.176.75:8086/"   // IP and port of the InfluxDB server
+#define INFLUXDB_TOKEN "jcD5WXc6SW22A4_E6gihv3g81tGSLFSzCeBnRHSAEFpuO2475Mo1k58GOhMPY4pBo9PHy2z07t8AINn96SYUrw=="    // API authentication token. Use an existing token or create a new one:
+                                                     //  InfluxDB UI -> Load Data -> API Tokens -> Generate API Token -> Custom API Token
+                                                     
+                                                     //  set a name for the token and select read and write permissions for your buket only!
+#define INFLUXDB_ORG "labiot-org"                    // organization id (Use: InfluxDB UI -> Profile -> About -> <name under organization profile> )
+#define INFLUXDB_BUCKET "MatteoComi-bucket"       // bucket name (Use: InfluxDB UI -> Load Data -> Buckets)
+
