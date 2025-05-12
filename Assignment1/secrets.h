448268c063961cd16969e440db82d514e4cc7e11
pegghe <m.comi15@campus.unimib.it>
2025-05-12 11:21:31 +0200

    Update secrets.h

--- Assignment1/secrets.h
+++ Assignment1/secrets.h
@@ -1,8 +1,7 @@
-// Use this file to store all of the private credentials and connection details
 
 // WiFi configuration
 #define SECRET_SSID "IoTLabThingsU14"                  // SSID
-#define SECRET_PASS "L@b%I0T*Ui4!P@sS**0%Lessons!"              // WiFi password
+#define SECRET_PASS "L@b%I0T*Ui4!P@sS**0%Lessons!"     // WiFi password
 
 // ONLY if static configuration is needed
 /*
@@ -19,10 +18,9 @@
 
 // InfluxDB cfg
 #define INFLUXDB_URL "http://149.132.176.75:8086/"   // IP and port of the InfluxDB server
-#define INFLUXDB_TOKEN "jcD5WXc6SW22A4_E6gihv3g81tGSLFSzCeBnRHSAEFpuO2475Mo1k58GOhMPY4pBo9PHy2z07t8AINn96SYUrw=="    // API authentication token. Use an existing token or create a new one:
-                                                     //  InfluxDB UI -> Load Data -> API Tokens -> Generate API Token -> Custom API Token
+#define INFLUXDB_TOKEN "jcD5WXc6SW22A4_E6gihv3g81tGSLFSzCeBnRHSAEFpuO2475Mo1k58GOhMPY4pBo9PHy2z07t8AINn96SYUrw=="    // API authentication token
                                                      
-                                                     //  set a name for the token and select read and write permissions for your buket only!
-#define INFLUXDB_ORG "labiot-org"                    // organization id (Use: InfluxDB UI -> Profile -> About -> <name under organization profile> )
+                                                     
+#define INFLUXDB_ORG "labiot-org"                 // organization id (Use: InfluxDB UI -> Profile -> About -> <name under organization profile> )
 #define INFLUXDB_BUCKET "MatteoComi-bucket"       // bucket name (Use: InfluxDB UI -> Load Data -> Buckets)
 
